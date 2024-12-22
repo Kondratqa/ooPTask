89064899480e17503124565145f06d7af5ccef2b
Kinomagnum <kinomagnum@gmail.com>
2024-12-22 22:04:52 +0300

    commit2

---
+++ src/main/java/busAndBusStation/Main.java
@@ -0,0 +1,4 @@
+package busAndBusStation;
+
+public class Main {
+}
---
+++ src/main/java/houseAndSeller/Main.java
@@ -0,0 +1,8 @@
+package houseAndSeller;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+    }
+}
